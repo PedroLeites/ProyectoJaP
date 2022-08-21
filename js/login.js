ca0f0638537b421e191fddaf85b38b536acb6948
PedroLeites <leitespsinsajo@gmail.com>
2022-08-21 10:50:48 -0300

    Entrega 1 Completa

---
+++ js/login.js
@@ -0,0 +1,36 @@
+function login() {
+  let mail = document.getElementById("email").value;
+  let pass = document.getElementById("password").value;
+  let bool = true;
+
+  if (mail.length < 1) {
+    alert("Falta email");
+    bool = false;
+  }
+
+  if (pass.length < 1) {
+    alert("Falta contraseña");
+    bool = false;
+  }
+
+  if (bool) {
+    window.location.href = "inicio.html";
+  }
+}
+/*
+let mail = document.getElementById("email").value;
+let pass = document.getElementById("password").value;
+let btnLogin = document.getElementById("btnLogin");
+
+btnLogin.addEventListener("click", login);
+
+function login() {
+  if (mail.length > 0 && pass.length > 0) {
+    console.log("hola");
+    window.location.href = "inicio.html";
+  } else {
+    console.log("Falta algo");
+    //window.location.href = "inicio.html";
+  }
+}
+*/
