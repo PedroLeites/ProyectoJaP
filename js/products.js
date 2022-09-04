66f2fdc20d03c7530354767b2cc86b8697d12bf0
PedroLeites <leitespsinsajo@gmail.com>
2022-09-04 20:41:49 -0300

    Corrección de products.js

--- js/products.js
+++ js/products.js
@@ -1,52 +1,89 @@
+/*
+
+let listaProductos = [];
+let min = undefined;
+let max = undefined;
+/*
+function filtarProductos(listaProductos) {
+  let listaFiltrada = filter(
+    (producto) =>
+      (parseInt(producto.cost) >= min || min == undefined) &&
+      (parseInt(producto.cost) <= max || max == undefined)
+  );
+  return listaFiltrada;
+}*/
+document.getElementById("nombreUsuario").innerHTML =
+  localStorage.getItem("usuario");
+
+let listado = [];
+let min = undefined;
+let max = undefined;
+let search = "";
+
 let catID = localStorage.getItem("catID");
-//console.log(catID);
-/*"https://japceibal.github.io/emercado-api/cats_products/101.json"*/
 const productosJSON =
   "https://japceibal.github.io/emercado-api/cats_products/" + catID + ".json";
 
-fetch(productosJSON)
-  .then((respuesta) => respuesta.json())
+function mostrarProductos(listaProductos) {
+  fetch(productosJSON)
+    .then((respuesta) => respuesta.json())
 
-  .then((datos) => {
-    let nombreCat = datos.catName;
-    //console.log(nombreCat);
-    document.getElementById("catNom").innerHTML = nombreCat;
+    .then((listaProductos) => {
+      let nombreCat = listaProductos.catName;
+      document.getElementById("catNom").innerHTML = nombreCat;
 
-    datos.products.forEach((producto) => {
-      let row = "";
-      row =
-        `<div class="list-group-item list-group-item-action cursor-active">
+      listaProductos.products.forEach((producto) => {
+        let row = "";
+        row =
+          `<div class="list-group-item list-group-item-action cursor-active">
       <div class="row">
         <div class="col-3">
           <img src="` +
-        producto.image +
-        `" alt="product image" class="img-thumbnail">
+          producto.image +
+          `" alt="product image" class="img-thumbnail">
         </div>
         <div class="col">
           <div class="d-flex w-100 justify-content-between">
             <div class="mb-1">
               <h4>` +
-        producto.name +
-        ` - ` +
-        producto.currency +
-        " " +
-        producto.cost +
-        `</h4>
+          producto.name +
+          ` - ` +
+          producto.currency +
+          " " +
+          producto.cost +
+          `</h4>
               <p>` +
-        producto.description +
-        `</p>
+          producto.description +
+          `</p>
             </div>
             <small class="text-muted">` +
-        producto.soldCount +
-        ` vendidos</small>
+          producto.soldCount +
+          ` vendidos</small>
           </div>
         </div>
       </div>
     </div>`;
-      document.getElementById("products-container").innerHTML += row;
-    });
-  })
-  .catch((error) => alert("Hubo un error" + error));
+        document.getElementById("products-container").innerHTML += row;
+      });
+    })
+    .catch((error) => alert("Hubo un error" + error));
+}
 
-document.getElementById("nombreUsuario").innerHTML =
-  localStorage.getItem("usuario");
+document.addEventListener("DOMContentLoaded", function () {
+  getJSONData(productosJSON).then((resultObj) => {
+    if (resultObj.status === "ok") {
+      listado = resultObj.data;
+      mostrarProductos(listado);
+    } else {
+      alert("Error");
+    }
+  });
+
+  document.getElementById("precioDesc").addEventListener("click", function () {
+    console.log("bonton desc ");
+    listado.sort(function (a, b) {
+      return parseInt(b.cost) - parseInt(a.cost);
+    });
+    mostrarProductos(listado);
+  });
+});
