ad64e9b2980a57996c2da0d815f1033cb4603984
PedroLeites <leitespsinsajo@gmail.com>
2022-09-16 00:20:14 -0300

    Agrego mostrar comentarios
    Con la función showProductComments() y la función showStars().

--- js/product-info.js
+++ js/product-info.js
@@ -33,6 +33,35 @@ function showProductInfo(){
   }
 }
 
+function showProductComments(){
+  commentsHTML = "";
+  for (let comment of commentsList){
+    commentsHTML += `
+    <li class="list-group-item">
+            <p class="mb-1"><span class="fw-bold">${comment.user}</span> &nbsp&nbsp ${comment.dateTime} &nbsp&nbsp ${showStars(comment.score)}</p>
+            <p class="mb-1">${comment.description}</p>
+        </li>
+    `
+  }
+  document.getElementById("Comments").innerHTML = commentsHTML;
+}
+
+function showStars(score){
+  let scoreHTML = "";
+  for (let i = 1; i <= 5; i++){
+    if (i <= score){
+      scoreHTML += `
+      <span class="fa fa-star checked"></span>
+      `
+    }else{
+      scoreHTML += `
+      <span class="fa fa-star"></span>
+      `
+    }
+  }
+  return scoreHTML;
+}
+
 document.addEventListener("DOMContentLoaded", function(e){
   let productID = localStorage.getItem("productID");
   getJSONData(PRODUCT_INFO_URL + productID + EXT_TYPE).then(function(resultObj){
